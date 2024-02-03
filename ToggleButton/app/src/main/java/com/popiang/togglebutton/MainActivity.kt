759b42c3957b51e9f2f149b65c39f082f86bc5a9
SHAHRIL MAD SHAH <popiang@hotmail.com>
2024-02-03 09:29:16 +0800

    adsf

---
+++ ToggleButton/app/src/main/java/com/popiang/togglebutton/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.popiang.togglebutton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.ToggleButton
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var image : ImageView
+    lateinit var result : TextView
+    lateinit var toggleButton : ToggleButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        image = findViewById(R.id.image)
+        result = findViewById(R.id.textResult)
+        toggleButton = findViewById(R.id.toggleButton)
+
+        toggleButton.setOnCheckedChangeListener { compoundButton, isChecked ->
+
+            if (isChecked) {
+                image.visibility = View.INVISIBLE
+                result.text = "The image is invisible"
+            } else {
+                image.visibility = View.VISIBLE
+                result.text = "The image is visible"
+            }
+        }
+    }
+}
